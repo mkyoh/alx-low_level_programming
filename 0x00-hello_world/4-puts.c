e1ac2833d02b1c5271ed20820c7031b6262af7dc
Mesmak <ymesmak@gmail.com>
2023-09-10 10:42:13 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline.
  * Return: 0 (success)
- */
+*/
 int main(void)
-{
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
