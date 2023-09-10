279433b454acdc20fc501a710b446e8c09c73c06
Mesmak <ymesmak@gmail.com>
2023-09-10 11:05:50 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-
 /**
  * main - Prints the size of various types based on
  * the computer it is compiled and run on ..
  * Return: Always 0.
- */
+*/
 int main(void)
 {
-	print("Size of a char: %zu byte(s)\n", sizeof(char));
-	print("Size of an int: %zu byte(s)\n", sizeof(int));
-	print("Size of a long int: %zu byte(s)\n", sizeof(long int));
-	print("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	print("Size of a float %zu byte(s)\n", sizeof(float));
-	return (0);
+print("Size of a char: %zu byte(s)\n", sizeof(char));
+print("Size of an int: %zu byte(s)\n", sizeof(int));
+print("Size of a long int: %zu byte(s)\n", sizeof(long int));
+print("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+print("Size of a float %zu byte(s)\n", sizeof(float));
+return (0);
 }
