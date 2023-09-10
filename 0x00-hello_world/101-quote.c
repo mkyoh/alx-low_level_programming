1ae1cd2c2fd625cbdd4dd0f9c3cc19d41ad7d08a
Mesmak <ymesmak@gmail.com>
2023-09-10 12:17:11 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19,
- * followed by a new line, to standard error.
- * Retur: Always 1.
+ * followed by a new line, to the standard error.
+ * Return: Always 1.
 */
 int main(void)
 {
