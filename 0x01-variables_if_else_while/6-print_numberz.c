7cfa4ef5a9441b54ea8a4a59f82428859e7129ec
Mesmak <ymesmak@gmail.com>
2023-09-12 14:38:10 -0700

    print alphabet 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,13 +2,13 @@
 #include <ctype.h>
 /**
 
-main - Entry point
-Return: Always 0(success)
-*/
+*main - Entry point
+*Return: Always 0(success)
+**/
 int main(void)
 {
 int num;
-for (num=0; num<10; num++)
+for (num = 0; num  < 10; num++)
 putchar((num % 10) + '0');
 putchar('\n');
 return(0);
