32e5665f84c741e135e4d373bbac5ee5a5ff223f
Mesmak <ymesmak@gmail.com>
2023-09-12 15:32:19 -0700

    print comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all the numbers in base 16 in lowercase.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+int i= '0'
+while(i<='9')
+{
+putchar(i);
+if (i!= '9')
+{
+putchar(',');
+putchar(' ');
+}
+++i;
+}
+putchar('\n');
+return(0);
+}
