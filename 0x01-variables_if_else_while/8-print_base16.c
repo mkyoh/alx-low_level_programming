7a334077b3330fde95ebe4e1e89de102ad884b2d
Mesmak <ymesmak@gmail.com>
2023-09-12 15:12:23 -0700

    print base 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,3 +8,12 @@
  */
 int main(void)
 {
+int num;
+char letter;
+for (num= '0'; num < 10; num++)
+putchar((num % 10) + '0'); 
+for (letter = 'a'; letter >= 'a'; letter--)
+putchar(letter);
+putchar('\n');
+return (0);
+}
