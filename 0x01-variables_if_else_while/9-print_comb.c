2033ae1276d2b4a9e89aed8bbb288b791145113d
Mesmak <ymesmak@gmail.com>
2023-09-12 15:42:06 -0700

    print comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 int i = '0';
-while (i<='9')
+while (i <= '9')
 {
 putchar(i);
-if (i!= '9')
+if (i != '9')
 {
 putchar(',');
 putchar(' ');
@@ -20,4 +20,4 @@ putchar(' ');
 }
 putchar('\n');
 return(0);
-i}
+}
