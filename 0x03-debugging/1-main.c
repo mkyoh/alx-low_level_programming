25de270eb78fdb840c7807748de8702c43516244
Mesmak <ymesmak@gmail.com>
2023-09-16 16:33:09 -0700

    1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,6 +12,7 @@ printf("Infinite loop incoming :(\n");
 * while (i < 10) 
 *{                                                                                *
 *putchar(i); 
+*}
 */
 printf("Infinite loop avoided! \\o/\n");
 return (0);
