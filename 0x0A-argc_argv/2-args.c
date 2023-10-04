3ad039d73f236a4eb9fc357e71c88333d3a3b846
Mesmak <ymesmak@gmail.com>
2023-10-04 08:24:16 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,10 +6,10 @@
  * @argv: array of arguments
  * Return: Always 0(success)
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
-for (i=0; i<argc; i++)
+for (i = 0; i < argc; i++)
 {
 	printf("%s\n", argv[i]);
 }
