e898c11f0cb3c7f8f83d78788bc29ba6773f4ac9
Mesmak <ymesmak@gmail.com>
2023-10-24 19:39:16 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,7 +7,7 @@
  *
  * Return: pointer to the node we're looking for, or NULL
  */
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int n)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int n = 0;
 listint_t *temp = head;
