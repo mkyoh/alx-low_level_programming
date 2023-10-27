2c77dd627f3acf15e6d0c6798d26a28fe6de9710
Mesmak <ymesmak@gmail.com>
2023-10-27 16:11:50 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,16 +9,17 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-        int alx;
-        unsigned int mesmak = 0;
+int alx;
+unsigned int mesmak = 0;
 
-        if (!b)
-                return (0);
-        for (alx = 0; b[alx]; alx++)
-        {
-                if (b[alx] < '0' || b[alx] > '1')
-                        return (0);
-                mesmak = 2 * mesmak + (b[alx] - '0');
-        }
-        return (mesmak);
+if (!b)
+return (0);
+for (alx = 0; b[alx]; alx++)
+{
+if (b[alx] < '0' || b[alx] > '1')
+return (0);
+mesmak = 2 * mesmak + (b[alx] - '0');
+}
+
+return (mesmak);
 }
