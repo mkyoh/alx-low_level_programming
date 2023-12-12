8e65e8a7c0819bbaae7b42fd38717909c25d7a0f
Mesmak <ymesmak@gmail.com>
2023-12-12 03:37:19 -0800

    add libraries

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _isupper - func check if uppercase letters
  * @c: char to check
@@ -6,7 +7,7 @@
  * Return:if uppercase return 1, else return 0
  */
 int _isupper(int c)
-0{
+{
 	if (c >= 'A' && c <= 'z')
 		return (1);
 
